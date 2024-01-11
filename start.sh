9aa9b8046607ac9e0587b0627db5747cf8eddebc
Aniket56829kw <132885070+Aniket56829kw@users.noreply.github.com>
2024-01-11 16:18:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/Aniket56829kw/Aniket-file-store-bot /Aniket-file-store-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /Aniket-file-store-bot
 fi
 cd /Advance-File-Share-Bot
 pip3 install -U -r requirements.txt
